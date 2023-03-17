afbcb828b1ce91374c0215508fc0bf67ae01af69
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-03-17 05:44:37 +0000

    Basic Features  -  Output and display an application heading

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("- - - - - - - - - - - - - -");
+Console.WriteLine("Bruno's Student-Marks-CO453");
+Console.WriteLine("- - - - - - - - - - - - - -");
