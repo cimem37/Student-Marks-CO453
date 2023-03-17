719597497337ab7a8cc45b943e2d17d00676e9be
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-03-17 09:57:38 +0000

    Basic Features covered
    Input a single mark for each of at least 10 students
    Display a list of the students together with their mark and their grade

--- Program.cs
+++ Program.cs
@@ -1,4 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("- - - - - - - - - - - - - -");
-Console.WriteLine("Bruno's Student-Marks-CO453");
-Console.WriteLine("- - - - - - - - - - - - - -");
+﻿using System;
+using System.ComponentModel;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] students = { "Hector", "Skyler", "Saul", "Huell", "Tuco", "Walter", "Jesse", "Gus", "Jane", "Hank" };
+        int[] grades = new int[10];
+
+        Console.WriteLine("- - - - - - - - - - - - - -");
+        Console.WriteLine("Bruno's Student-Marks-CO453");
+        Console.WriteLine("- - - - - - - - - - - - - -");
+        Console.WriteLine("*ᴾʳᵉˢˢ ᵃⁿʸ ᵏᵉʸ ᵗᵒ ᶜᵒⁿᵗᶦⁿᵘᵉ*");
+        Console.ReadKey();
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.Write($"Enter {students[i]}'s grade: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out grades[i]) || grades[i] < 0 || grades[i] > 100)
+            {
+                Console.WriteLine("[ERROR] 0 to 100 input only accepted");
+                i--; // repeat this iteration
+            }
+        }
+
+
+        Console.WriteLine("Would you like to see the grades displayed? (Y/N)");
+        string showGrades = Console.ReadLine();
+
+        if (showGrades.ToUpper() == "Y")
+        {
+            Console.WriteLine("\nGrades:");
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.Write($"{students[i]} = {grades[i]} / ");
+
+                if (grades[i] >= 70)
+                {
+                    Console.WriteLine("A");
+                }
+                else if (grades[i] >= 60)
+                {
+                    Console.WriteLine("B");
+                }
+                else if (grades[i] >= 50)
+                {
+                    Console.WriteLine("C");
+                }
+                else if (grades[i] >= 40)
+                {
+                    Console.WriteLine("D");
+                }
+                else
+                {
+                    Console.WriteLine("F");
+                }
+            }
+        }
+
+
+
+        Console.WriteLine("*ᴾʳᵉˢˢ ᵃⁿʸ ᵏᵉʸ ᵗᵒ ᵉˣᶦᵗ*");
+        Console.ReadKey();
+    }
+}
+
